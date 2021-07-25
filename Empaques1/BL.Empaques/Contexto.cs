4a2e1212ecd4fb964da53b308f5e393cc5c7ff93
Josue <Josue@DESKTOP-85QRENU>
2021-07-25 17:46:44 -0600

    Guardar Cambios

---
+++ Empaques1/BL.Empaques/Contexto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Empaques
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(): base("MaterialesDeEmpaque")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new  Datosdeinicio()); // Agrega datos de inicio a la base de datos después de eliminarla
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
